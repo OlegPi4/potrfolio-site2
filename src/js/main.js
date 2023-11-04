c49984b65f99c648842c9257f15ae7f512971d50
OlegPichkur <oleg1pichkur@gmail.com>
2023-11-04 12:42:34 +0200

    адаптация story video

--- src/js/main.js
+++ src/js/main.js
@@ -1,127 +1,19 @@
-/*-----------Функции-------------*/
-
-/*Адаптация блока Story после загрузки документа*/
-function StoryVideo() {
-   const screenWidth = window.screen.width;
-   const videoFrame = document.querySelector('.video-place');
-   let widthFrameStr = '896px',
-   heightFrameStr = '516px'
-   if ( (screenWidth > 1050)&&(screenWidth <= 1500)) {
-      widthFrameStr = Math.round(screenWidth/1.719) + 'px';
-      heightFrameStr = Math.round(screenWidth/2.98) + 'px';
-   
-   }  else if (screenWidth <= 1050) {
-      if ((screenWidth - 30) / 1.1 <= 896) {
-         widthFrameStr = Math.round((screenWidth - 30) / 1.1 ) + 'px';
-         heightFrameStr = Math.round((screenWidth - 30) / 1.1 / 1.78 ) + 'px';
-      }
-   }
-   videoFrame.style.cssText = `
-      width: ${widthFrameStr};
-      height: ${heightFrameStr};
-      `;   
-}
-
-/*Адаптация блока Story - видеофрагмента на изменение размера окна*/
-function adaptStoryVideo() {
-
-   const screenWidth = window.screen.width;
-   const videoFrame = document.querySelector('.video-place');
-   let widthFrameStr = '896px',
-      heightFrameStr = '516px',
-      widthFrame = 906,
-      heightFrame = 532
-   
-   if (+videoFrame.dataset.video == 1) {
-      if ( (screenWidth > 1050)&&(screenWidth <= 1500)) {
-         widthFrame = Math.round(16+screenWidth/1.719);
-         heightFrame = Math.round(22+screenWidth/2.98);
-         widthFrameStr = Math.round(screenWidth/1.719) + 'px';
-         heightFrameStr = Math.round(screenWidth/2.98) + 'px';
-      }  else {
-         if (screenWidth <= 1050) {
-            widthFrame = Math.round((12+screenWidth - 30) / 1.1);
-            heightFrame = Math.round((18+screenWidth - 30) / 1.1 / 1.78);
-            widthFrameStr = Math.round((screenWidth - 30) / 1.1) + 'px';
-            heightFrameStr = Math.round((screenWidth - 30) / 1.1 / 1.78) + 'px';
-         }
-      }
-   videoFrame.style.cssText = `
-      width: ${widthFrameStr};
-      height: ${heightFrameStr};
-      `;   
-   videoFrame.innerHTML = `
-      <video  autoplay controls width='${widthFrame}' height='${heightFrame}' src="../dist/video/HistoricalDrawings.mp4"></video>;
-      `;
-   } else {
-
-      if ( (screenWidth > 1050)&&(screenWidth <= 1500)) {
-         console.log('adept 1050-1500');
-         widthFrameStr = Math.round(16+screenWidth/1.719) + 'px';
-         heightFrameStr = Math.round(22+screenWidth/2.98) + 'px';
-         
-      }  else  {
-
-         if (screenWidth <= 1050) {
-            console.log('adept < 1050 w<896');
-            widthFrameStr = Math.round(12+(screenWidth - 30) / 1.1) + 'px';
-            heightFrameStr = Math.round(14+(screenWidth - 30) / 1.1 / 1.78) + 'px';
-         } 
-      }   
-      videoFrame.style.cssText += `
-         width: ${widthFrameStr};
-         height: ${heightFrameStr};
-         `;
-   }
-}
-
- /*Запуск видеофрагмента блока Story*/
-function videoPlay () {
-   const screenWidth = window.screen.width;
-   const videoFrame = document.querySelector('.video-place');
-   let widthFrame = 906,
-      heightFrame = 538,
-      widthFrameStr = '896px',
-      heightFrameStr = '516px'
-   if ( (screenWidth > 1050)&&(screenWidth <= 1500)) {
-      widthFrame = Math.round(12+screenWidth/1.719);
-      heightFrame = Math.round(14+screenWidth/2.98);
-      widthFrameStr = Math.round(screenWidth/1.719) + 'px';
-      heightFrameStr = Math.round(screenWidth/2.98) + 'px';
-   
-   }  else {
-
-      if (screenWidth <= 1050) {
-         widthFrame = Math.round(12+(screenWidth - 30) / 1.1);
-         heightFrame = Math.round(14+(screenWidth - 30) / 1.1 / 1.78);
-         widthFrameStr = Math.round((screenWidth - 30) / 1.1 ) + 'px';
-         heightFrameStr = Math.round((screenWidth - 30) / 1.1 / 1.78 ) + 'px';
-      }
-   }
-  
-   videoFrame.style.cssText = +`
-      width: ${widthFrameStr};
-      height: ${heightFrameStr};
-      `;  
-   videoFrame.innerHTML = `
-      <video  autoplay controls width='${widthFrame}' height='${heightFrame}' src="../dist/video/HistoricalDrawings.mp4"></video>;
-      `;
-   videoFrame.dataset.video = 1
-}
-
 
 /*---------События------------*/
 
 /*Загрузка документа адаптация блока Story*/
-window.addEventListener('DOMContentLoaded', (e) => {
-   //e.stopPropagation();
-   adaptStoryVideo();
+window.addEventListener('DOMContentLoaded', () => {
+   adaptStoryFrame();
 })
 /*Изменение размера окна адаптация блока Story*/
-window.addEventListener('resize', (e) => {
-   e.stopPropagation();
-   adaptStoryVideo();
+window.addEventListener('resize', () => {
+   adaptStoryFrame();
 })
+/*Завершение медиафайла*/
+// video.addEventListener(ended, () => {
+//    console.log('midia-ended');
+// })
+
 
 /*Запуск видеофрагмента блока Story при нажатии на кнопку "play"*/
 let elemPlay = document.querySelector('.video__a__play')
@@ -129,6 +21,62 @@ elemPlay.addEventListener('click', videoPlay)
 /*Запуск видеофрагмента блока Story при нажатии на кнопку "lets get started"*/
 let elemButton = document.querySelector('.story__a')
 elemButton.addEventListener('click', videoPlay)   
+
+/*-----------Функции-------------*/
+
+
+/*Адаптация блока Story - видеофрагмента на изменение размера окна*/
+function adaptStoryFrame() {
+   const docWidth = document.documentElement.clientWidth
+   const videoFrame = document.querySelector('.video-place');
+   let widthFrameStr = '896px',
+      heightFrameStr = '516px',
+      widthFrame = 896,
+      heightFrame = 516
+   if ((docWidth > 1031)&&(docWidth <= 1500)) {
+      widthFrame = Math.round(docWidth/1.719);
+      heightFrame = Math.round(docWidth/2.98);
+      widthFrameStr = widthFrame + 'px';
+      heightFrameStr = heightFrame + 'px';
+   }  else if (docWidth <= 1031) {
+      widthFrame = Math.round((docWidth - 30) / 1.1);
+      heightFrame = Math.round((docWidth - 30) / 1.1 / 1.78);
+      widthFrameStr = widthFrame + 'px';
+      heightFrameStr = heightFrame + 'px';
+   }
+   
+   videoFrame.style.cssText += `
+      width: ${widthFrameStr};
+      height: ${heightFrameStr};
+      `;   
+
+   
+   if (videoFrame.dataset.video == 1) { 
+      // let video = document.getElementsByTagName('video');
+      // console.log(video);
+      // video = video[0];
+      // console.log(video);
+      // console.log(video.getAttribute(attributeName));
+      // video.setAttribute(width, widthFrame); 
+      // video.setAttribute(height, heightFrame); 
+      // }
+
+      videoFrame.innerHTML = `
+      <video id="video" autoplay controls width='${widthFrame}' height='${heightFrame}' src="../dist/video/HistoricalDrawings.mp4"></video>;
+      `;
+   }
+}
+   
+ /*Запуск видеофрагмента блока Story*/
+function videoPlay () {
+   const videoFrame = document.querySelector('.video-place');
+   videoFrame.dataset.video = 1
+   // videoFrame.innerHTML = `
+   //    <video id="video" autoplay controls width='896' height='516' src="../dist/video/HistoricalDrawings.mp4"></video>;
+   //    `;
+   adaptStoryFrame();
+}
+
 
 /*Закрытие видеофрагмента по щелчку вне видео*/
 // document.querySelector('body').onclick = function(e) {
